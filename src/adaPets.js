0f1bea38072ce89976a6dcaf63181dd2f470006c
Dianna Tingg <dtingg@gmail.com>
2019-11-18 14:37:38 -0800

    updated remove pet

--- src/adaPets.js
+++ src/adaPets.js
@@ -44,6 +44,13 @@ const removePet = (selectedPet) => {
   }
 
   // Fill out as part of Wave 3.
+  axios.delete(`${BASE_URL}${selectedPet}`)
+  .then((response) => {
+    setResult("Pet successfully removed.")
+  })
+  .catch((error) => {
+    setError("Error: Failed to remove pet.")
+  });
 }
 
 const addPet = (petInfo) => {
@@ -57,12 +64,3 @@ module.exports = {
   removePet: removePet,
   addPet: addPet
 }
-
-
-// hould setResult and object with details for the pet and should call setError with an error message if the request fails.
-// You can run the Wave 2 tests with jest test/wave2.test.js. Once these are passing move on to Wave 3.
-// Function to complete
-// •	showDetails
-// o	setResult should be passed the Object that represents the pet.
-// o	setError should be passed an error message. (You may need to write this.)
-// 	The tests for this wave are looking for an error message that will include two specific words inside of the string. (It does this with Regex). Part of this wave is to read through the tests and determine what two words should be inside of the error message string. What two words are expected to be in this error message? Work with a neighbor on this in order to find out.
